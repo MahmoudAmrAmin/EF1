18d90a706008af575cc499640e7028eab6655cb0
Mahmoud Amr <mahmoudamr500@gmail>
2024-09-19 03:21:58 +0300

    add all files

---
+++ EF-SetupEFCoreModle/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace EF_SetupEFCoreModle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder ()
+                                .AddJsonFile("appSettings.json").
+                                Build ();
+
+            var connectionString = configuration.GetSection("constr").Value;
+
+            Console.WriteLine(connectionString);
+
+
+
+        }
+    }
+}
